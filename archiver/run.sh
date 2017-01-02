e60711553384a530f7426ad340556e9a1865b2ed
Ryan Hefner <rhefner1@gmail.com>
2017-01-02 18:40:55 -0500

    In notebook, change scans to assets

--- archiver/run.sh
+++ archiver/run.sh
@@ -3,10 +3,10 @@
 cd ~/Dropbox/notebook/journal
 
 # Fix links to other documents
-ls -t .. | grep -v scans | tr '\n' '|' | head -c -1 | grep -PRoh "\.\.\/($(cat -))" | uniq | sed 's#\.#\\\.#g' | while read -r line; do sed -i "s#$line#\.\./$line#g" *.md; done
+ls -t .. | grep -v assets | tr '\n' '|' | head -c -1 | grep -PRoh "\.\.\/($(cat -))" | uniq | sed 's#\.#\\\.#g' | while read -r line; do sed -i "s#$line#\.\./$line#g" *.md; done
 
 # Move linked scans to archive/scans
-grep -PRoh "\.\.\/scans\/(.*?)\.pdf" | while read -r line; do mv "$line" ../archive/scans; done
+grep -PRoh "\.\.\/assets\/(.*?)\.pdf" | while read -r line; do mv "$line" ../archive/assets; done
 
 # Move all files to archive/journal
 mv ./*.md ../archive/journal/
--- orphaned-scans/run.sh
+++ orphaned-scans/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-find /home/rhefner/Dropbox/notebook/scans/ -name *.pdf | xargs -d '\n' python find_orphaned_scans.py
-find /home/rhefner/Dropbox/notebook/archive/scans/ -name *.pdf | xargs -d '\n' python find_orphaned_scans.py
+find /home/rhefner/Dropbox/notebook/assets/ -name *.pdf | xargs -d '\n' python find_orphaned_scans.py
+find /home/rhefner/Dropbox/notebook/archive/assets/ -name *.pdf | xargs -d '\n' python find_orphaned_scans.py
 
